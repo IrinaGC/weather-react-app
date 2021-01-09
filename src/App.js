74902d9f3cde72522606221fdba585d41ab2541b
IrinaGC <59023713+IrinaGC@users.noreply.github.com>
2021-01-09 22:19:52 +0000

    Added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   <div className="App">
     <MainCard />
     <p className="footer" >
-      <a href="https://github.com/IrinaGC/weather-react-app.git">
+      <a href="https://github.com/IrinaGC/weather-react-app.git" rel="noreferrer" target="_blank">
         Open - source code </a>
-      by Irina G.C. at <a href="https://www.shecodes.io/" >
+      by Irina G.C. at <a href="https://www.shecodes.io/" rel="noreferrer" target="_blank">
         SheCodes </a>
     </p >
   </div>
