d71f8064a67fcdc563581f59bb7382c9c0afd91e
IrinaGC <59023713+IrinaGC@users.noreply.github.com>
2021-01-21 13:26:41 +0000

    changed file type from .js to .jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from "./components/App.js";
+import App from "./components/App.jsx";
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
