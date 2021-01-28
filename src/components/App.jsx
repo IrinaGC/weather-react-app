1f0172e27aa194d95a1ab40815fad7ce0c4487f8
IrinaGC <59023713+IrinaGC@users.noreply.github.com>
2021-01-28 12:05:41 +0000

    Edited the footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,11 +12,10 @@ function App() {
         </div>
       </div>
     <p className="footer" >
-      <a href="https://github.com/IrinaGC/weather-react-app.git" rel="noreferrer" target="_blank">
-        Open-sourced </a>
-      by Irina G.C. at <a href="https://www.shecodes.io/" rel="noreferrer" target="_blank">
-        SheCodes </a>
-    </p >
+       React project coded by <a href="https://www.linkedin.com/in/irinacirlan/" rel="noreferrer" target="_blank">Irina Cirlan</a>, open-sourced on <a href="https://github.com/IrinaGC/weather-react-app.git" rel="noreferrer" target="_blank">
+        GitHub</a>,
+        hosted on <a href="https://epic-wright-87a1e6.netlify.app/" rel="noreferrer" target="_blank"> Netlify </a> 
+    </p>
     </div>
   </div>
   );
